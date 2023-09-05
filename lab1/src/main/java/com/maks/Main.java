b44ba7d94575882bbdf187f7174634f6d66fd2cb
Yarema Maksym <macsym2@gmail.com>
2023-09-05 17:53:51 +0300

    feat: complete lab1

--- lab1/src/main/java/com/maks/Main.java
+++ lab1/src/main/java/com/maks/Main.java
@@ -2,6 +2,6 @@ package com.maks;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello from Yarema Maksym form KI-306");
     }
 }
