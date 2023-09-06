bbb0e0786dd62440bdc335da521448e5e587a28d
Yarema Maksym <macsym2@gmail.com>
2023-09-06 14:07:06 +0300

    feat(lab4): add documentation for CommanderEquipment and CommanderGasMask

--- lab4/src/main/java/ki306/yarema/lab4/models/CommanderEquipment.java
+++ lab4/src/main/java/ki306/yarema/lab4/models/CommanderEquipment.java
@@ -1,6 +1,13 @@
 package ki306.yarema.lab4.models;
 
 public interface CommanderEquipment {
+    /**
+     * Returns the type of equipment of the commander".
+     */
     String getEquipmentType();
+
+    /**
+     * Sends an SOS signal in case of an emergency.
+     */
     void sendSosSignal();
 }
--- lab4/src/main/java/ki306/yarema/lab4/models/CommanderGasMask.java
+++ lab4/src/main/java/ki306/yarema/lab4/models/CommanderGasMask.java
@@ -1,12 +1,28 @@
 package ki306.yarema.lab4.models;
 
+/**
+ * The CommanderGasMask class represents the commander's gas mask,
+ * which implements the CommanderEquipment interface and inherits the properties of the GasMask class.
+ *
+ * @author Yarema Maksym
+ * @version 1.0
+ * @since version 1.0
+ */
 public class CommanderGasMask extends GasMask implements CommanderEquipment {
 
+    /**
+     * Returns the type of equipment of the commander - "Gas mask".
+     *
+     * @return type of commanders gas mask
+     */
     @Override
     public String getEquipmentType() {
         return getType();
     }
 
+    /**
+     * Sends an SOS signal in case of an emergency.
+     */
     @Override
     public void sendSosSignal() {
         log("Sending SOS signal...");
