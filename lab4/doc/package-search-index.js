338f9bac8fd79addf1293cb6f12bbaeb06e51855
Yarema Maksym <macsym2@gmail.com>
2023-09-06 14:21:14 +0300

    feat(lab4): generate JavaDoc

---
+++ lab4/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ki306.yarema.lab4"},{"l":"ki306.yarema.lab4.models"}];updateSearchResults();
---
+++ lab4/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ki306.yarema.lab4.models","l":"CommanderEquipment"},{"p":"ki306.yarema.lab4.models","l":"CommanderGasMask"},{"p":"ki306.yarema.lab4.models","l":"Filter"},{"p":"ki306.yarema.lab4.models","l":"GasMask"},{"p":"ki306.yarema.lab4","l":"GasMaskApplication"}];updateSearchResults();
