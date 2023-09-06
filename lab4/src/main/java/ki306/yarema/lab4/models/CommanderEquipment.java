b71effcc1c7da2d4ffd0bd76bf2148519ef3dbcc
Yarema Maksym <macsym2@gmail.com>
2023-09-06 14:13:31 +0300

    feat(lab4): add documentation for CommanderEquipment interface itself

--- lab4/src/main/java/ki306/yarema/lab4/models/CommanderEquipment.java
+++ lab4/src/main/java/ki306/yarema/lab4/models/CommanderEquipment.java
@@ -1,5 +1,10 @@
 package ki306.yarema.lab4.models;
 
+/**
+ * The CommanderEquipment interface represents the equipment used by the commander.
+ * All classes that implement this interface must provide methods for getting the type of equipment
+ * and sending an SOS signal.
+ */
 public interface CommanderEquipment {
     /**
      * Returns the type of equipment of the commander".
