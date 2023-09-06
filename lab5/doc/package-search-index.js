138ed2a539a81653bbc0fd08c8d5c61fe1f2be32
Yarema Maksym <macsym2@gmail.com>
2023-09-06 23:09:39 +0300

    feat(lab5): generate JavaDoc

---
+++ lab5/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ki306.yarema.lab5"}];updateSearchResults();
---
+++ lab5/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ki306.yarema.lab5","l":"CalcException"},{"p":"ki306.yarema.lab5","l":"Equation"},{"p":"ki306.yarema.lab5","l":"EquationsApp"}];updateSearchResults();
