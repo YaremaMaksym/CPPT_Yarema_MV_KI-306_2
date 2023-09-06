9731aa3d02f4f2d43444c170a9741219ef872d8d
Yarema Maksym <macsym2@gmail.com>
2023-09-06 17:13:12 +0300

    feat(lab5): implemented functionality to calculate equation

---
+++ lab5/src/main/java/ki306/yarema/lab5/CalcException.java
@@ -0,0 +1,10 @@
+package ki306.yarema.lab5;
+
+public class CalcException extends ArithmeticException{
+    public CalcException(){}
+    public CalcException(String cause)
+    {
+        super(cause);
+    }
+
+}
