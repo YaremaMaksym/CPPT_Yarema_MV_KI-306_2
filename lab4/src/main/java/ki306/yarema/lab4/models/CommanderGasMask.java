bf4dce54776e4c97cfebc5099c5354db918de19a
Yarema Maksym <macsym2@gmail.com>
2023-09-06 14:05:43 +0300

    ref(lab4): rename ComanderGasMask -> CommanderGasMask

--- lab4/src/main/java/ki306/yarema/lab4/models/CommanderGasMask.java
+++ lab4/src/main/java/ki306/yarema/lab4/models/CommanderGasMask.java
@@ -1,6 +1,7 @@
 package ki306.yarema.lab4.models;
 
-public class ComanderGasMask extends GasMask implements CommanderEquipment {
+public class CommanderGasMask extends GasMask implements CommanderEquipment {
+
     @Override
     public String getEquipmentType() {
         return getType();
