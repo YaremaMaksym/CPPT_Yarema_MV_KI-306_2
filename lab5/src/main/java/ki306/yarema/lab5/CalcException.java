1c33d9bdab4bfc939be606b5fdcf70d7924bd3dd
Yarema Maksym <macsym2@gmail.com>
2023-09-06 17:16:05 +0300

    feat(lab5): add documentation to CalcException

--- lab5/src/main/java/ki306/yarema/lab5/CalcException.java
+++ lab5/src/main/java/ki306/yarema/lab5/CalcException.java
@@ -1,5 +1,11 @@
 package ki306.yarema.lab5;
 
+/**
+ * Class CalcException more precises ArithmeticException
+ * @author Yarema Maksym
+ * @version 1.0
+ */
+
 public class CalcException extends ArithmeticException{
     public CalcException(){}
     public CalcException(String cause)
