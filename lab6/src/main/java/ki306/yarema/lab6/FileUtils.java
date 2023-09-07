70a69d9c655c5ebc39e631fc706a1b0d786e0f2a
Yarema Maksym <macsym2@gmail.com>
2023-09-07 11:08:48 +0300

    ref(lab6): add documentation for FileUtils

--- lab6/src/main/java/ki306/yarema/lab6/FileUtils.java
+++ lab6/src/main/java/ki306/yarema/lab6/FileUtils.java
@@ -4,7 +4,18 @@ import java.io.*;
 import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * The FileUtils class provides utility methods for reading and writing double values to files.
+ * @author Yarema Maksym
+ * @version 1.0
+ */
 public class FileUtils {
+    /**
+     * Writes a double value to a text file.
+     *
+     * @param fName   The name of the file to write to.
+     * @param result  The double value to write to the file.
+     */
     public void writeResTxt(String fName, double result) {
         try {
             PrintWriter f = new PrintWriter(fName);
@@ -16,6 +27,12 @@ public class FileUtils {
         }
     }
 
+    /**
+     * Reads a double value from a text file.
+     *
+     * @param fName  The name of the file to read from.
+     * @return       The double value read from the file, or 0 if the file does not contain a valid double value.
+     */
     public double readResTxt(String fName) {
         double result = 0;
         try {
@@ -35,6 +52,12 @@ public class FileUtils {
         return result;
     }
 
+    /**
+     * Writes a double value to a binary file.
+     *
+     * @param fName   The name of the file to write to.
+     * @param result  The double value to write to the file.
+     */
     public void writeResBin(String fName, double result) {
         try {
             DataOutputStream f = new DataOutputStream(new FileOutputStream(fName));
@@ -47,6 +70,12 @@ public class FileUtils {
         }
     }
 
+    /**
+     * Reads a double value from a binary file.
+     *
+     * @param fName  The name of the file to read from.
+     * @return       The double value read from the file, or 0 if the file does not contain a valid double value.
+     */
     public double readResBin(String fName) {
         double result = 0;
         try {
